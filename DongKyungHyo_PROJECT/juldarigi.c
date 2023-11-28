b9621d811a1bef0b231e57d0d4be14502b8632a0
Nhyojeong <hyo8890@naver.com>
2023-11-29 00:49:52 +0900

    기본 조작키, 눕기 추가 완료 / 출력 오류, 규칙 수정, 코드 정리 필요

--- DongKyungHyo_PROJECT/juldarigi.c
+++ DongKyungHyo_PROJECT/juldarigi.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <time.h>
+#include <conio.h>
+#include <windows.h>
 #include "jjuggumi.h"
 #include "canvas.h"
 #include "keyin.h"
@@ -14,7 +16,11 @@ int key;
 int hx[3], hy[3];
 int p, al, di, nu = 0;
 int nupgi_turns = 0;
-int bh;
+int count = 0;
+int one = 0, two = 0;
+int before_str = 0;
+int dx, dy, nx, ny, d;
+int buhwal;
 
 void juldarigi_init();
 void p_str();
@@ -80,19 +86,19 @@ void p_str() { //str 계산
 void move_player(int player, int nx, int ny) { //플레이어
 	int p = player;  // 이름이 길어서...
 	back_buf[ny][nx] = back_buf[py[p]][px[p]];
-	back_buf[py[p]][px[p]] = '  ';
+	back_buf[py[p]][px[p]] = ' ';
 	px[p] = nx;
 	py[p] = ny;
 }
 
-void move_dash(int ddx, int ddy, int i) { //플레이어 출력
+void move_dash(int ddx, int ddy, int i) { //줄
 	back_buf[ddx][ddy] = back_buf[hy[i]][hx[i]];
 	back_buf[hy[i]][hx[i]] = ' ';
 	hx[i] = ddx;
 	hy[i] = ddy;
 }
 
-void player_print() { //줄
+void player_print() { //플레이어 출력
 	for (int i = 0; i < n_player; i++) {
 		if (player[i].is_alive == true) {
 			back_buf[py[i]][px[i]] = '0' + i;
@@ -122,9 +128,41 @@ void after_nupgi() {
 	nupgi_turns = 0;
 }
 
+void countkey() {
+	while (1) {
+		if (_kbhit()) {
+			char key = _getch();
+
+			if (key == 'z') {
+				count++;
+				one = 1;
+			}
+			else if (key == '/') {
+				count++;
+				two = 1;
+			}
+		}
+		Sleep(500);
+		if (!_kbhit()) {
+			break;
+		}
+	}
+	return 0;
+}
+
 void play_juldarigi() {
+	before_str = str;
 	int nx, ny;
+	countkey();
 	p_str();
+	if (one == 1 || two == 1) {
+		if (one == 1) {
+			str -= count;
+		}
+		else if (two == 1) {
+			str += count;
+		}
+	}
 	Sleep(1000);
 	for (int i = 0; i < n_player; i++) {
 		p = i;
@@ -136,9 +174,6 @@ void play_juldarigi() {
 			}
 			move_player(p, nx, ny);
 			player_print();
-			/*if (nu != 0 && nupgi_turns == 1) {
-				after_nupgi();
-			}*/
 		}
 		else if (str < 0) {
 			nx = px[p] - 1;
@@ -148,58 +183,57 @@ void play_juldarigi() {
 			}
 			move_player(p, nx, ny);
 			player_print();
-			/*if (nu != 0 && nupgi_turns == 1) {
-				after_nupgi();
-			}*/
 		}
-		if (px[p] == N_COL / 2 - 1 && player[p].is_alive) {
+		if ((p % 2 == 0 && px[p] >= 15) || (p % 2 != 0 && px[p] <= 15)) {
 			if (p % 2 == 0) {
 				al = 1;
 			}
 			else if (p % 2 != 0) {
 				al = 2;
 			}
-			di = 1, out_p = 1;
-			back_buf[py[p]][px[p]] = ' ';
-			out_player_jul[out_p] = '0' + p;
+			di = 1;
+			back_buf[1][15] = ' ';
+			out_player_jul[out_p] = '0' + i;
 			player[i].is_alive = false;
 			n_alive -= 1;
-			out_p = i;
-		}
-		if (nu != 0 && nupgi_turns == 1) {
-			after_nupgi();
 		}
 	}
-	if (al == 1 || al == 2) {
+	if (al == 1 || al == 2) { //죽었을때 상대편 안움직이게 할 코드
 		for (int i = 0; i < n_player; i++) {
 			if (i % 2 == 0 && al == 2) {
-				int nx, ny;
 				nx = px[i] + 1;
 				ny = 1;
-				move_tail(i, nx, ny);
+				move_player(i, nx, ny);
 				player_print();
 			}
 			else if (i % 2 != 0 && al == 1) {
-				int nx, ny;
 				nx = px[i] - 1;
 				ny = 1;
-				move_tail(i, nx, ny);
+				move_player(i, nx, ny);
 				player_print();
 			}
 		}
-		for (int j = 0; j < 3; j++) {
-			int dx, dy;
+		for (int j = 0; j < 3; j++) { //죽었을때 줄 안움직이게 하는 코드
+			d = j;
 			dx = hx[j];
 			dy = hy[j];
+			if (nu != 0 && nupgi_turns == 1) {
+				if (str > 0) {
+					dx += 1;
+				}
+				else if (str < 0) {
+					dx -= 1;
+				}
+			}
 			move_dash(dx, dy, j);
 			dash_print();
 		}
 		al = 0;
 	}
-	else {
+	else { //죽은애 없을때 줄 움직이게 하는 코드
 		int moveAmount = (str > 0) ? 1 : -1;
 		for (int j = 0; j < 3; j++) {
-			int dx, dy;
+			d = j;
 			dx = hx[j] + moveAmount;
 			dy = hy[j];
 			if (nu != 0 && nupgi_turns == 1) {
@@ -214,6 +248,10 @@ void play_juldarigi() {
 			dash_print();
 		}
 	}
+	if (nu != 0 && nupgi_turns == 1) {
+		after_nupgi();
+	}
+	count = 0;
 }
 
 int juldarigi(void) {
@@ -232,25 +270,43 @@ int juldarigi(void) {
 			int key = _getch();
 			if (key == 'q') { break; }
 			else if (key == 'x') {
+				int ifff = 0;
 				nu = 1;
 				gotoxy(0, 4);
 				printf("왼쪽 팀이 눕기를 사용했습니다.\n");
 				for (int i = 0; i < n_player; i++) {
-					if (i % 2 == 0) {
+					if (i % 2 == 0 && player[i].stamina > 0) {
 						player[i].str *= 2;
 						player[i].stamina *= 0.7;
 					}
+					else if (i % 2 == 0 && player[i].stamina < 0) {
+						ifff = 1;
+					}
+				}
+				if (ifff == 1) {
+					gotoxy(0, 4);
+					printf("왼쪽 팀이 눕기를 사용했지만, 스테미너가 부족하여 실패하였습니다.\n");
+					ifff = 0;
 				}
 				nupgi_turns = 1;
 			}
 			else if (key == '.') {
+				int ifff = 0;
 				gotoxy(0, 4);
 				printf("오른쪽 팀이 눕기를 사용했습니다.\n");
 				for (int i = 1; i < n_player; i++) {
-					if (i % 2 != 0) {
+					if (i % 2 != 0 && player[i].stamina > 0) {
 						player[i].str *= 2;
 						player[i].stamina *= 0.7;
 					}
+					else if (i % 2 != 0 && player[i].stamina < 0) {
+						ifff = 1;
+					}
+				}
+				if (ifff == 1) {
+					gotoxy(0, 4);
+					printf("왼쪽 팀이 눕기를 사용했지만, 스테미너가 부족하여 실패하였습니다.\n");
+					ifff = 0;
 				}
 				nupgi_turns = 1;
 				nu = 2;
@@ -281,10 +337,27 @@ int juldarigi(void) {
 		printf("str :	%d\n", str);
 		Sleep(1000);
 		if (di == 1) {
-			dialog_juldarigi("player", "dead!", out_p, 2);
+			dialog_juldarigi("player", "dead!", out_player_jul, out_p);
 			di = 0;
 		}
-		if (n_alive == 1) {
+		if (one == 1 || two == 1) {
+			str = before_str;
+			one = 0;
+			two = 0;
+		}
+		int allEvenDead = 1; // 짝수 애들 죽었는지 확인할 코드
+		int allOddDead = 1;  //홀수 애들 
+
+		for (int i = 0; i < n_player; i++) {
+			if (i % 2 == 0 && player[i].is_alive) {
+				allEvenDead = 0;
+			}
+			else if (i % 2 != 0 && player[i].is_alive) {
+				allOddDead = 0;
+			}
+		}
+
+		if (allEvenDead || allOddDead) {
 			break;
 		}
 	}
